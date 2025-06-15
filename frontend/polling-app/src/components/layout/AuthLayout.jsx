b680f81c919c48ec73e2ceb5ea467559ee63c846
unknown <laxmanlucky437727@gmail.com>
2025-06-16 02:26:07 +0530

    commit change

--- frontend/polling-app/src/components/layout/AuthLayout.jsx
+++ frontend/polling-app/src/components/layout/AuthLayout.jsx
@@ -10,12 +10,12 @@ const AuthLayout = ({children}) => {
     <div>
         <div className='flex'>
             <div className='w-screen h-screen md:w-1/2 px-12 pt-8 pb-12'>
-                <h2 className='text-lg font-medium tet-black'>Polling App</h2>
+                <h2 className='text-lg font-bold text-black'>Poll Quest</h2>
                 {children}
             </div>
             <div className='hidden md:block w-1/2 h-screen bg-sky-50  bg-cover bg-no-repeat bg-center overflow-hidden relative'>
-                <img src={UI_ELEMENT} className='w-[50%] absolute right-0 top-14'/>
-                <img src={UI_ELEMENT} className='w-[55%] rotate-180 absolute left-0 -bottom-[20%]'/>
+                <img src={UI_ELEMENT} className='w-[55%] absolute right-0 '/>
+                <img src={UI_ELEMENT} className='w-[55%] rotate-180 absolute left-0 -bottom-[1%]'/>
 
                 <img src={CARD_1} className='w-64 lg:w-72 absolute top-[8%] left-[10%] shadow-lg shadow-blue-400/15'/>
                 <img src={CARD_2} className='w-64 lg:w-72 absolute top-[44%] left-[44%] shadow-lg shadow-blue-400/15'/>
